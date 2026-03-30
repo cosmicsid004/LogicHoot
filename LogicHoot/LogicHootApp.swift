d28fbde9802fe1987a20a21d0a32b0ef364f6087
Siddharth <siddharthdream2004@Gmail.com>
2026-03-30 12:04:37 +0530

    Initial Commit

---
+++ LogicHoot/LogicHootApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogicHootApp.swift
+//  LogicHoot
+//
+//  Created by Siddharth on 30/03/26.
+//
+
+import SwiftUI
+
+@main
+struct LogicHootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
